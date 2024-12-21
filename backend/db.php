76e6814974d703617916aa53570c2e037f476957
818MF <raishatim8@gmail.com>
2024-12-21 20:28:50 +0100

    3HatimChanges

--- backend/db.php
+++ backend/db.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
-$host = '127.0.0.1'; 
-=======
 $host = 'localhost:3306'; 
->>>>>>> dd9e6bf69907c8ee074aa246e526b3fea90bba3f
 $dbname = 'newlearn'; 
 $username = 'root'; // Your MySQL username
 $password = ''; // Your MySQL password
